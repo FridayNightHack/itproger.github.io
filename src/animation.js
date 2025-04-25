52825cda571dee22a95aa5754d1d76fc019f11be
sam <ackerlite623@gmail.com>
2025-04-25 17:09:41 +0500

    25.08.2025:+Enhanced the latest

---
+++ src/animation.js
@@ -0,0 +1,21 @@
+const benefits = document.querySelectorAll('.benefits')[0];
+const startLine = document.getElementById('benefitsStart');
+
+window.addEventListener('scroll', () => {
+    if(document.documentElement.offsetHeight >= benefits.getBoundingClientRect().top + startLine.offsetTop + startLine.offsetHeight) {
+        const benefitsList = document.querySelectorAll('.benefits-list-item');
+
+        Array.from(benefitsList).forEach((item, index) => {
+            let icon = item.querySelectorAll('.benefits-list-icon');
+            let text = item.querySelectorAll(['.benefits-header', 'benefits-notice-title > output']);
+
+        Array.from(icon).forEach((iconItem) => {
+            iconItem.style.animation = `icon-scale 0.5s ease-in-out 0.${index}s forwards`;
+            })
+
+            Array.from(text).forEach((item) => {
+                item.style.animation = `from-right 0.5s ease-in-out 0.${index}s forwards`;
+            })
+        })
+    }
+})
