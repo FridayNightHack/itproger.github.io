f735b5193fd0a5a2585f868802a004ade1660fa6
sam <ackerlite623@gmail.com>
2025-05-05 17:02:49 +0500

    5.05.2025: +Full ended

--- src/animation.js
+++ src/animation.js
@@ -1,22 +1,85 @@
-const benefits = document.querySelectorAll('.benefits')[0];
-const startLine = document.getElementById('benefitsStart');
+const animate = [
+    {
+        id:'benefits',
+        prntBlck: '.benefits', 
+        strtLne: 'startLine1',
+        nodeBlck: '.benefits-list-item',
+        animate: {
+            icon: '.benefits-list-icon',
+            text: '.benefits-header'
+        },
+        animation: 'icon-scale 0.5s ease-in-out'
+    },    
+    {
+        id: 'capabilities',
+        prntBlck: '.capabilities', 
+        strtLne: 'startLine2',
+        nodeBlck: '.capabilities-item',
+        animate: null,
+        animation: '1'
+    },
+    {
+        id: 'work-process',
+        prntBlck: '.work-process', 
+        strtLne: 'startLine3',
+        nodeBlck: '.work-stages-item',
+        animate: {
+            icon: '.icon-scale',
+
+        },
+        animation: 'from-bottom-opacity 0.9s ease-in-out'
+    },
+]
 
 window.addEventListener('scroll', () => {
-    if(document.documentElement.offsetHeight >= benefits.getBoundingClientRect().top + startLine.offsetTop + startLine.offsetHeight) {
-        const benefitsList = document.querySelectorAll('.benefits-list-item');
 
-        Array.from(benefitsList).forEach((item, index) => {
-            let icon = item.querySelectorAll('.benefits-list-icon');
-            let text = item.querySelectorAll(['.benefits-header', 'benefits-notice-title > output']);
+    animate.forEach((mainBlock) => {
 
-        Array.from(icon).forEach((iconItem) => {
-            iconItem.style.transform = 'scale(1, 1)';
-            iconItem.style.animation = `icon-scale 0.5s ease-in-out`;
+        const prntBlck = document.querySelector(mainBlock.prntBlck);
+        const strtLne = document.getElementById(mainBlock.strtLne);
+
+        console.log(document.documentElement.offsetHeight +":"+ (prntBlck.getBoundingClientRect().top + +strtLne.offsetTop + (+strtLne.offsetHeight / 2)))
+        if(document.documentElement.offsetHeight >= (prntBlck.getBoundingClientRect().top + +strtLne.offsetTop + (+strtLne.offsetHeight / 2))) {
+            const itemList = document.querySelectorAll(mainBlock.nodeBlck);
+
+            Array.from(itemList).forEach((item, index) => {
+                animateFunc(item, index, mainBlock?.animate, mainBlock?.animation);
+            })
+            
+        }
+
+    })
+
+   
+})
+
+const animateFunc = (elem, index, hasElem, animation) => {
+    
+        if(hasElem === null) {
+           return elem.style.opacity = animation
+        }
+            const icon = elem.querySelectorAll(hasElem.icon);
+            const text = elem.querySelectorAll(hasElem.text);
+
+            Array.from(icon).forEach((iconItem) => {
+                iconItem.style.transform = 'scale(1, 1)';
+                // iconItem.style.animation = animation;
             })
 
             Array.from(text).forEach((item) => {
                 item.style.animation = `from-right 0.5s ease-in-out 0.${index}s forwards`;
             })
-        })
-    }
+        
+}
+
+
+
+const accordion = document.getElementById('accordion');
+const accordionItems = accordion.querySelectorAll('.accordion-item');
+Array.from(accordionItems).forEach((item) => {
+    item.addEventListener('click', () => {
+        const answer = item.querySelectorAll('.acc-answer')[0];
+        
+            answer.style.maxHeight ? answer.style.maxHeight = null : answer.style.maxHeight = answer.scrollHeight + 'px';
+    })
 })
